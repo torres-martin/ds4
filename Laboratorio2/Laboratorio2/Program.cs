9055f8ab9d7acd48d121e04e4674f5809436aaa8
Martin Torres <martintorres0078@gmail.com>
2025-09-22 19:40:06 -0500

    Lab2

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de clase.
+            client.FirstName = "Martin";
+            client.LastName = "Torres";
+            client.Age = 20;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+    public class Client
+    {        
+        //Declarando variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetFullName()
+        {
+            //Utilizando las variables de instancia dentro de metodos de la clase.
+            return FirstName + " " + LastName;
+        }
+    }
+
+}
---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Asignado valor a variable estastica.
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando variable estatica.
+        public static int Valor;
+    }
+}
